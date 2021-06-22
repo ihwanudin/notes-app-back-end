a06ff6bf9486771d33b5f0cccd603c97b88808ba
ihwanudin <ihwanoedin@gmail.com>
2021-06-22 15:51:16 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const init = async () => {
 
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.21.72',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
